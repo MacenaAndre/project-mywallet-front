65f0fbff0f512a6cbcfaeebda8bc840f58d5f2e0
MacenaAndre <andremacenacosta@gmail.com>
2022-09-06 03:28:31 -0300

    Starting Project

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStyles from "./styled-components/GlobalStyles"
+
+export default function App () {
+    return (
+        <>
+            <div>olá</div>
+        </>
+    );
+        
+}
